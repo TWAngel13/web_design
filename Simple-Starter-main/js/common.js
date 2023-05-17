a4736315f78bc8e07f6ca604b37509e629fe97dc
Airplane <letadllo@gmail.com>
2023-05-17 17:47:08 +0500

    До конца

--- Simple-Starter-main/js/common.js
+++ Simple-Starter-main/js/common.js
@@ -1,1 +1,17 @@
-// $('body').hide()
+
+
+document.getElementById('orderButton').onclick = () =>
+{
+    if (!document.getElementById('nameInput').value || !document.getElementById('phoneInput').value || !document.getElementById('emailInput').value)
+    {
+        alert('Пожалуйста, заполните все поля')
+    }
+    else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('emailInput').value))
+    {
+        alert('Ваша заявка принята!');
+    }
+    else
+    {
+        alert('"' + document.getElementById('emailInput').value + '" не является корректным адресом электронной почты');
+    }
+}
